e4d3bfd323cbf7240d039d6619b0d0fc31ddce55
eronitplakiq <eplakiqi14@gmail.com>
2022-06-20 15:17:36 +0200

    profa suggestion commit:)

--- app/middlewares/adminOnly.js
+++ app/middlewares/adminOnly.js
@@ -1,21 +1,13 @@
-const User = require('./../models/User');
-
 module.exports = (req, res, next) => {
-    User.findOne({ _id: req.user.id }).then((user, err) => {
-        if(user.role === 'admin'){
-            next();
-        }else{
-            res.status(403).json({
-                errors: { 
-                    global: 'You are unauthorized for this action',
-                    error: err
-                }
-            })
-        }
-    }).catch(error => {
-        res.status(500).json({
-            msg: "Server Error",
-            error
+    
+    if(req.user.role === 'admin'){
+        next();
+    }else{
+        res.status(403).json({
+            errors: { 
+                global: 'You are unauthorized for this action',
+                error: err
+            }
         })
-    })
+    }
   };
